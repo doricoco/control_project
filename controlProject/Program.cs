59ecddb56a80f0f9c418d194f18bacb404a91b5d
doricoco <balchugova.darya03@ya.ru>
2022-09-19 23:34:59 +0300

    Добавлен код программы

---
+++ controlProject/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] wordsArray = { "water", "handler", "some", "cringe", "gb", "sun", "god" };
+
+string[] resultArray = new string[wordsArray.Length];
+
+for(int i = 0; i < wordsArray.Length; i++)
+{
+    if (wordsArray[i].Length <= 3)
+        resultArray[i] = wordsArray[i];
+}
